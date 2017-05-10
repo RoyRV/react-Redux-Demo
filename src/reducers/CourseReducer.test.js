46718eafb5d2f742020f210053dc08f8d4b029ba
rrojas <rrojas@RROJAS>
2017-05-10 10:38:55 -0500

    add new test case

--- src/reducers/CourseReducer.test.js
+++ src/reducers/CourseReducer.test.js
@@ -19,4 +19,22 @@ describe('Course Reducer',()=>{
         expect(newState[1].title).toEqual('B');
         expect(newState[2].title).toEqual('C');
     });
+    it('should update course when passed update_course_success',()=>{
+        //arrange
+        const initialState = [
+            { id : 'A',title : 'A'},
+            { id : 'B',title : 'B'},
+            { id : 'C',title : 'C'}
+        ];
+        const course = {id : 'B', title : 'New Title'};
+        const action = actions.updateCourseSuccess(course);
+        //act
+        const newState = courseReducer(initialState,action);
+        const updatedCourse= newState.find(c=>c.id==course.id);
+        const untouchedCourse= newState.find(c=>c.id=='A');
+        //assert
+        expect(newState.length).toEqual(3);
+        expect(updatedCourse.title).toEqual('New Title');
+        expect(untouchedCourse.title).toEqual('A');
+    });
 });
